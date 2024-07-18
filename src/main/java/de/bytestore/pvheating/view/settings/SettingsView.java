9e71b707de3654dd7c94b07d6751712d07184255
Jan Heil <janheil@outlook.de>
2024-07-18 18:07:09 +0200

    Add tabs and icons to settings view
    The code has been updated to add multiple tabs to the settings view for better user navigation. New SVG icons for temperature, transistor, and solar have been added to the frontend resources. Also, added translations for the new UI elements for the German and English languages.

--- src/main/java/de/bytestore/pvheating/view/settings/SettingsView.java
+++ src/main/java/de/bytestore/pvheating/view/settings/SettingsView.java
@@ -1,14 +1,23 @@
 package de.bytestore.pvheating.view.settings;
 
 
+import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.router.Route;
 import de.bytestore.pvheating.view.main.MainView;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.component.tabsheet.JmixTabSheet;
+import io.jmix.flowui.view.*;
 
 @Route(value = "settings-view", layout = MainView.class)
 @ViewController("heater_SettingsView")
 @ViewDescriptor("settings-view.xml")
 public class SettingsView extends StandardView {
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        initTabs();
+    }
+
+    private void initTabs() {
+    }
+
 }
