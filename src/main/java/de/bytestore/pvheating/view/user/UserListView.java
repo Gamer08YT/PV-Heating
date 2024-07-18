7d823bc740f0a652812a5a0f7aeb537f0d104388
Jan Heil <janheil@outlook.de>
2024-07-18 14:03:41 +0200

    Add significant frontend and backend functionalities
    This commit introduces a comprehensive set of features for both the frontend and backend of the application. Frontendwise, the changes involve setting up the base template of the file index.html, introducing a CSS file for theming, and generating user views. On the backend, it sets up the database schema for user management, provides password encoding, and configures related services. In summary, these changes lay the foundation for the initial structure and functionality of the application.

---
+++ src/main/java/de/bytestore/pvheating/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package de.bytestore.pvheating.view.user;
+
+import de.bytestore.pvheating.entity.User;
+import de.bytestore.pvheating.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("heater_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
